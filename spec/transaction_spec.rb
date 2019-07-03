6874905f3ceeb76369527d74c70990fce7972f6d
Francesca Chater <francescachater@gmail.com>
2019-07-03 11:16:46 +0100

    Refactored transaction credit test and withdraw test passing

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -8,28 +8,29 @@ describe 'Transaction' do
   transaction = Transaction.new
 
   before do
-    allow(bank).to receive(:deposit)
-    allow(bank).to receive(:deposit).with(1000, :balance).and_return(1000, 1000)
+    allow(bank).to receive(:deposit).with(1000).and_return(1000, 1000)
+    allow(bank).to receive(:withdraw).with(500).and_return(500, 500)
   end
 
   describe 'credit account' do
     it 'records date of deposit, the amount paid and updated balance' do
       Timecop.freeze(Time.local(2012, 0o1, 10))
-      bank.deposit(1000, :balance)
+      bank.deposit(1000)
 
       expect(transaction.credit(1000, 1000)).to eq([
                                                      '10/01/2012', format('%.2f', 1000), '', format('%.2f', 1000)
                                                    ])
     end
   end
+
+  describe 'debit account' do
+    it 'records date of withdrawal, the amount taken and updated balance' do
+      Timecop.freeze(Time.local(2012, 0o1, 14))
+      bank.withdraw(500)
+     
+      expect(transaction.debit(500, 500)).to eq([
+                                                     '14/01/2012', '', format('%.2f', 500), format('%.2f', 500)
+                                                   ])
+    end
+  end
 end
-
-# RSpec.describe "and_call_original" do
-#     it "can be overriden for specific arguments using #with" do
-#       allow(Calculator).to receive(:add).and_call_original
-#       allow(Calculator).to receive(:add).with(2, 3).and_return(-5)
-
-#       expect(Calculator.add(2, 2)).to eq(4)
-#       expect(Calculator.add(2, 3)).to eq(-5)
-#     end
-#   end
